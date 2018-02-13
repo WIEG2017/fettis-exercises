2eea04e381c6ef9922af36b1db6c370dae771a88
mehri <faroch.mehri@gmail.com>
2018-02-13 15:02:25 +0100

    Klart med issue #1

---
+++ phpversion.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="sv"><head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>PHP Version</title>
+</head><body>
+    <h1>Current PHP version: <?=phpversion();?></h1>
+</body>
+</html>
