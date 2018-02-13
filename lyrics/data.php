3d6038433ecc61a868012f591b02c0993c01224e
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-02-13 14:55:08 +0100

    Update

---
+++ lyrics/data.php
@@ -0,0 +1,296 @@
+<?php 
+
+$songs = array(
+    "Red nation"=>"Throw your muthafucking Cincinnati hats in the sky
+    Nigga don't ask why
+    Red laces in and out of them Air Max nine fives I,
+    Walk on the moon, flow hotter than June
+    Any nigga want drama I kick up a sand dune
+    Peace to my man 'Tune for giving his man room
+    Now we hittin' switches to the Spring Break, Cancun
+    Get it, nah forget it, soo woo I live it
+    Made the letter B more famous than a Red Sox fitted
+    But that was suicide, I don't live in Judah's eyes
+    Half of these rappers weren't trappin' when I was choppin' the do or die
+    Suge had me in, I went Puffy like Zab Judah eye
+    Dre called, told my baby momma won't you decide
+    She chose Doc, first day I pulled Jude aside like its Aftermath for life
+    And all I do is ride
+    Before I turn on him I kill Satan
+    And stick my red flag in the ground it's red nation!
+    
+    Now blood the fuck up
+    Everyday's a gamble motherfucker, tough luck
+    And we gon' fuck the world til that bitch bust nuts
+    I can't tell ya whats good, but I can tell ya whats, what
+    And that's, B's up, hoes down
+    Lookin' in the mirror, I'm no where to be found
+    Blood, I'm a dog, call me a blood hound
+    Throwin' blood in the air, leave blood on the ground
+    
+    Niggas'll trade they soul to be Drake or J. Cole
+    Live and die for this shit, word to Tupac Shakur's halo
+    One blood, plural, nigga I'm spendin' Euro's
+    Ferrari got an ice cream paint job, Dorrough
+    I'm up out the hood, where they pull guns on you like
+    Come up out ya hood, it ain't never all good
+    We roll up in backwoods, nigga get to actin' stupid
+    Get thrown in the back woods
+    Los Angeles, home of the scandalous
+    Pimp, hoes and gamblers
+    Ninety eight degree's on Christmas
+    Nigga we rollin' cannibus
+    Swisha sweet ain't it, I told her I'm Charles Louboutin
+    The bitch fainted, pulled her panites down, stain it
+    That's my Chi-lingo, yeah I'm bi-lingual
+    Ball by myself nigga Ochocinco
+    Dancing with the stars, bullets and fast cars
+    And everybody bleed out here, word to God
+    
+    Now blood the fuck up
+    Everyday's a gamble motherfucker, tough luck
+    And we gon' fuck the world til that bitch bust nuts
+    I can't tell ya whats good, but I can tell ya whats, what
+    And that's, B's up, hoes down
+    Lookin' in the mirror, I'm no where to be found
+    Blood, I'm a dog, call me a blood hound
+    Throwin' blood in the air, leave blood on the ground
+    
+    Russia got a Red Flag
+    US got Red Stripes
+    Last train to Paris, round the world in these red Nikes
+    Che Guevara of the new era, test me
+    Louisville slugger, you'll get buried in my New Era
+    Got that natty on tighter than a magnum
+    Walk in the club saggin' with a thirty eight magnum
+    Red Ralph Laurens, the double R sittin' on a hill like Lauren
+    Her and the car foreign
+    Got my red Dre Beats on, tryna put my peeps on
+    And I keep it hood like this Phantom is a Nissan
+    Where my nigga Jim Jones at?
+    Roll up the weed son, so many bloods in Compton had to get a NYC song
+    And while I'm out here, I might as well go shopping
+    And put this new bad bitch I got her some red bottoms
+    And all these hatin' ass niggas want me dead
+    Cause I'm Malcolm X before he turned Muslim, red!
+    
+    Now blood the fuck up
+    Everyday's a gamble motherfucker, tough luck
+    And we gon' fuck the world til that bitch bust nuts
+    I can't tell ya whats good, but I can tell ya whats, what
+    And that's, B's up, hoes down
+    Lookin' in the mirror, I'm no where to be found
+    Blood, I'm a dog, call me a blood hound
+    Throwin' blood in the air, leave blood on the ground",
+
+    "Hallelujah"=>"Well I heard there was a secret chord
+    That David played and it pleased the Lord
+    But you don't really care for music, do you?
+    Well it goes like this: the fourth, the fifth
+    The minor fall and the major lift
+    The baffled king composing Hallelujah
+    Hallelujah [x4]
+    Your faith was strong but you needed proof
+    You saw her bathing on the roof
+    Her beauty and the moonlight overthrew you
+    She tied you to her kitchen chair
+    She broke your throne and she cut your hair
+    And from your lips she drew the Hallelujah
+    Hallelujah [x4]
+    Baby I've been here before
+    I've seen this room and I've walked this floor (you know)
+    I used to live alone before I knew you
+    And I've seen your flag on the marble arch
+    And love is not a victory march
+    It's a cold and it's a broken Hallelujah
+    Hallelujah [x4]
+    There was a time when you let me know
+    What's really going on below
+    But now you never show that to me, do you?
+    But remember when I moved in you
+    And the holy dove was moving too
+    And every breath we drew was Hallelujah
+    Hallelujah [x4]
+    Maybe there's a God above
+    All I've ever learned from love
+    Was how to shoot somebody who outdrew you
+    And it's not a cry that you hear at night
+    It's not somebody who's seen the light
+    It's a cold and it's a broken Hallelujah
+    Hallelujah [x13]",
+
+    "Thunderstruck"=>"Thunder, thunder, thunder, thunder
+    I was caught
+    In the middle of a railroad track
+    I looked round
+    And I knew there was no turning back
+    My mind raced
+    And I thought what could I do
+    And I knew
+    There was no help, no help from you
+    Sound of the drums
+    Beating in my heart
+    The thunder of guns
+    Tore me apart
+    You've been
+    Thunderstruck
+
+    Rode down the highway
+    Broke the limit, we hit the town
+    Went through to Texas, yeah Texas, and we had some fun
+    We met some girls
+    Some dancers who gave a good time
+    Broke all the rules
+    Played all the fools
+    Yeah yeah they, they, they blew our minds
+    And I was shaking at the knees
+    Could I come again please
+    Yeah them ladies were too kind
+    You've been
+    Thunderstruck
+
+    I was shaking at the knees
+    Could I come again please
+
+    Thunderstruck, Thunderstruck, Thunderstruck, Thunderstruck
+    It's alright, we're doin' fine
+    It's alright, we're doin' fine, fine, fine
+    Thunderstruck, yeah, yeah, yeah
+    Thunderstruck, Thunderstruck
+    Thunderstruck, baby, baby
+    Thunderstruck, you've been Thunderstruck
+    Thunderstruck, Thunderstruck
+    You've been Thunderstruck",
+
+    "Kryptonite"=>"I took a walk around the world to
+    Ease my troubled mind
+    I left my body lying somewhere
+    In the sands of time
+    I watched the world float to the dark
+    Side of the moon
+    I feel there is nothing I can do, yeah
+    
+    I watched the world float to the
+    Dark side of the moon
+    After all I knew it had to be something
+    To do with you
+    I really don't mind what happens now and then
+    As long as you'll be my friend at the end
+    
+    If I go crazy then will you still
+    Call me Superman
+    If I'm alive and well, will you be
+    There holding my hand
+    I'll keep you by my side with
+    My superhuman might
+    Kryptonite
+    
+    You called me strong, you called me weak
+    But still your secrets I will keep
+    You took for granted all the times I
+    Never let you down
+    You stumbled in and bumped your head, if
+    Not for me then you'd be dead
+    I picked you up and put you back
+    On solid ground
+    
+    If I go crazy then will you still
+    Call me Superman
+    If I'm alive and well, will you be
+    There holding my hand
+    I'll keep you by my side with
+    My superhuman might
+    Kryptonite
+    
+    Oh whoa whoa
+    
+    If I go crazy then will you still
+    Call me Superman
+    If I'm alive and well, will you be
+    There holding my hand
+    I'll keep you by my side with
+    My superhuman might
+    Kryptonite
+    
+    If I go crazy then will you still
+    Call me Superman
+    If I'm alive and well, will you be
+    There holding my hand
+    I'll keep you by my side with
+    My superhuman might
+    Kryptonite",
+
+    "Love story"=>"When this well runs dry
+    I guess you'll find another well
+    When I say goodbye
+    There'll be another breeze to sail
+    When my boat goes down
+    I guess you'll learn how to swim
+    If I don't come home
+    Just know you've been a good friend
+    Don't forget me
+    I love you so
+    Don't forget me
+    I've never let you go
+    Don't forget me
+    Love is the story I wrote
+    Don't forget me
+    Don't forget me
+    If God is my angel, the fucking devil's the pistol
+    Better put your face behind safety glass when I load up this missile
+    Whistling past this infinite path, I limp when I drag
+    My footprints like a giant, look at this mountain
+    Bounce on my boots and then climb it
+    Mount up my troupes and defiantly
+    Go against what they trying
+    Teach 'em to shoot when I'm rhyming
+    Be living proof of my word
+    Meaning I'm Bible designing
+    Jesus I'm rival, decapitating put the bait on my line and
+    Throw the hook to the open water
+    Fish till I catch that motherfucker
+    Looking for Megalodon, Goliath, Leviathan
+    I have been dying to find him and tie him to my boat and chop him up
+    And dine with him on a plate with a steak and a coke
+    Rely on patience and hope
+    Speak to a nation, it shows
+    Reach through these speakers and grab you
+    Turn the bass up and then choke
+    And I earned my place on this road
+    Ain't nobody moving me, no way
+    No, how, OK, go, pow
+    When I was born they said exorcise
+    And the hospital set on fire
+    The preacher got goosebumps when he saw me and said Messiah
+    Here from the heavens, I have arose from the ashes of hell
+    Brought here to be poor white trash and excel
+    Stuck in the woods of 'Bama, wood like meth on the shelf
+    A scientist under development with nines and twelves and threes
+    And things like pyramids and fuck it you'll see
+    How I made a pot of gold from a bucket of beans
+    How I made it back to dry land, stuck in the sea
+    From crippled ships to triple dip Z's 71's
+    Yeah it's already begun
+    And I ain't even had a hit but I'm fuckin' ready for one
+    I got my weight up
+    Like I'm carrying fat people
+    I planned it, I landed
+    Touch down, that Seagull
+    But above all the guts glory it's more than just blood for me
+    My heart is half open for the ones who never gave a fuck for me
+    My love story
+    Don't forget me (yeah, yeah)
+    Don't forget me (yeah, yeah)
+    Don't forget me (yeah, yeah)
+    Don't forget me (yeah, yeah)
+    Don't forget me (yeah, yeah)
+    Don't forget me (yeah, yeah)
+    Don't forget me (yeah, yeah)
+    Don't forget me (yeah, yeah)
+    Don't forget me
+    Don't forget me
+    Don't forget me
+    Don't forget me"
+    );
+
+?>
