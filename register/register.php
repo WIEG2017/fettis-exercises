71ec5acc4835780cc6608d94f33e08750470b4fd
andremedie <32762205+andremedie@users.noreply.github.com>
2018-02-13 14:40:45 +0100

    register update

--- register/register.php
+++ register/register.php
@@ -1,3 +1,16 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1> Please Write your Name and Lastname 👽 </h1>
+    <h4>And see what happens when you click 'Submit'</h4>
 <?php 
 
 echo "<form action='results.php' METHOD='POST'>";
@@ -9,4 +22,12 @@ echo "<input type='text' name='lastname'>Lastname<br/>";
 echo "<input type='submit' value='Submit'>";
 
 echo     "</form>";
+
+
+
+
 ?>
+
+
+</body>
+</html>
